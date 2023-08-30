cb54c4e2258b23a8f01c177076e9686bc511cafa
Your Name <you@example.com>
2023-08-30 15:01:32 -0400

    changes

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("theme-toggle");
+    const body = document.body;
+
+    themeToggle.addEventListener("click", () => {
+        console.log("Button Clicked!");
+        body.classList.toggle("night-theme");
+    });
+});
