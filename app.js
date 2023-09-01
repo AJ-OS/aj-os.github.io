3cf73646b24f3fe4a355bbebf96b734cf578c938
AJ <morrisalan671@gmail.com>
2023-09-01 18:03:42 -0400

    fix-1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+// button
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggle = document.getElementById("theme-toggle");
     const body = document.body;
-
     themeToggle.addEventListener("click", () => {
         console.log("Button Clicked!");
         body.classList.toggle("night-theme");
