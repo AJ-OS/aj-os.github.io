3573a5c17b6199deffb1b76f4500cfd1862fadc3
AJ <morrisalan671@gmail.com>
2023-09-01 20:06:09 -0400

    contact form fixes

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-// button
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggle = document.getElementById("theme-toggle");
+    const themeIcon = document.getElementById("theme-icon");
     const body = document.body;
+
+    let isNightTheme = false;
+
     themeToggle.addEventListener("click", () => {
         console.log("Button Clicked!");
+
+        
         body.classList.toggle("night-theme");
+
+        if (isNightTheme) {
+            themeIcon.src = "/Icons/5ea56758df7bd.png";
+        } else {
+            themeIcon.src = "/Icons/sun-1.png";
+        }
+
+        // Update the theme state
+        isNightTheme = !isNightTheme;
     });
 });
-
